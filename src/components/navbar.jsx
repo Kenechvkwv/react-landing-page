e7f2f2109b0dc0881f0c78c81b9554380f9c1119
kenechvkwv <ubesiekene@gmail.com>
2022-09-23 16:01:58 +0100

    2nd commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import logo from "../images/logo.png";
+
+function Navbar() {
+  return (
+    <nav className={""}>
+      <a href="" className="logo">
+        <img src={logo} alt="" />
+      </a>
+      <input type="checkbox" className="menu-btn" id="menu-btn" />
+      <label className="menu-icon" for="menu-btn">
+        <span className="nav-icon"></span>
+      </label>
+
+      <ul className="menu">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">Features</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">UI SS</a>
+        </li>
+        <li>
+          <a href="#">Download</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
