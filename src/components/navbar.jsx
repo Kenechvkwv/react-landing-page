d74ca2ed4a3118b91655450137a72cd7e47da874
kenechvkwv <ubesiekene@gmail.com>
2022-09-23 16:30:06 +0100

    landing page bg added with navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 
 function Navbar() {
+  const [nav, setnav] = useState(false);
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true);
+    } else {
+      setnav(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackground);
+
   return (
-    <nav className={""}>
+    <nav className={nav ? "nav-active" : "nav"}>
       <a href="" className="logo">
         <img src={logo} alt="" />
       </a>
@@ -14,7 +25,9 @@ function Navbar() {
 
       <ul className="menu">
         <li>
-          <a href="#">Home</a>
+          <a href="#" className="active">
+            Home
+          </a>
         </li>
         <li>
           <a href="#">Features</a>
